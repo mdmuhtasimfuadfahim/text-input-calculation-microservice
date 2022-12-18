78e46839f11bf9d2eec045c4af76ac8fef87ac7a
Muhtasim Fahim <muhtasim.fahim.98@gmail.com>
2022-12-18 18:38:04 +0600

    fixes-completed

--- screen-a/src/messagebroker/config/RegisteredTopic.js
+++ screen-a/src/messagebroker/config/RegisteredTopic.js
@@ -1,6 +1,7 @@
 const RegisteredTopics = {
   ALL_INPUTS: 'ALL_INPUTS',
   ALL_OUTPUTS: 'ALL_OUTPUTS',
+  CALCULATION_RESULT: 'CALCULATION_RESULT',
 };
 
 module.exports = RegisteredTopics;
--- screen-a/src/services/file.service.js
+++ screen-a/src/services/file.service.js
@@ -10,65 +10,6 @@ const lettersNumbersSpacesDashes = async (fileData) => {
   return /^[A-Za-z0-9 -]*$/.test(fileData);
 }
 
-const isNumeric = async (str) => {
-  if (typeof str != "string") return false
-  return !isNaN(str) && !isNaN(parseFloat(str))
-}
-
-const isOperand = async (token) => {
-  const ops = ["add", "multiply", "minus", "divide"]
-  if (ops.includes(token)) {
-    return true;
-  }
-
-  return false;
-}
-
-const interpret = async (input) => {
-  const inputData = input.split(' ');
-  let state = 0;
-
-  for (i = 0; i < inputData.length; i++) {
-    const t = inputData[i] // to keep things shorter
-    if (!isOperand(t)) {
-      throw new ApiError(3002, `Epected Operand Data, Got: ${t}`)  
-    }
-
-    let a, b;
-
-    const next = inputData[i + 1]
-    if (next == "by") {
-      a = state
-      b = parseFloat(inputData[i + 2])
-      i += 2
-    } else if (isNumeric(next)) {
-      const and = inputData[i + 2] // this should be the "and"
-      if (and != "and") {
-        throw new ApiError(3002, `Expected "and", Got: ${and}`)
-      }
-      a = parseFloat(next)
-      b = parseFloat(inputData[i + 3])
-      i += 3
-    } else {
-      throw new Error(3002, `Unexpected: ${next}`)  
-    }
-
-    switch (t) {
-      case "add": 
-        state = a + b
-        break;
-      case "multiply":
-        state = a * b
-      case "divide":
-        state = a / b
-      case "minus":
-        state = a - b
-    }
-  }
-
-  return state
-}
-
 /**
  * Fetch file data
  * @param {String} filename
